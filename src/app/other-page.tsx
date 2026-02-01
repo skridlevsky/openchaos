965c0317d8f233d6b0e5ee38548e8f5462ab59a3
Nickolas Høyer-Larsen <nickhoyer@me.com>
2026-02-01 10:40:23 -0300

    hehe

--- src/app/other-page.tsx
+++ src/app/other-page.tsx
@@ -1,10 +1,6 @@
 import { IE6Layout } from "@/components/IE6Layout";
 
 export default function OtherPage() {
-  if (Math.random() <= 0.01337) {
-    return null;
-  }
-
   return (
     <IE6Layout>
       <h1>Secret Message Page</h1>
