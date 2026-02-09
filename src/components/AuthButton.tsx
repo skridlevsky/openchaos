ab95a569a038c9bb7a7603b2460036ed61fdd5a3
Matt Mayer <matt@matthewmayer.co.uk>
2026-02-09 08:45:07 +0700

    fix auth button

--- src/components/AuthButton.tsx
+++ src/components/AuthButton.tsx
@@ -23,7 +23,7 @@ export function AuthButton() {
 
   return (
     <button onClick={login} className="auth-button">
-      <b>[Login with GitHub]</b>
+      [Login with GitHub]
     </button>
   );
 }
